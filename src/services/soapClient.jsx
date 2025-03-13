79be667b55c1de77a70b9e317c38bb5a0b17dd7a
Ajeeth <ajeetheswari14@gmail.com>
2025-03-13 11:01:41 +0530

    netlify proxy removed

--- src/services/soapClient.jsx
+++ src/services/soapClient.jsx
@@ -2,19 +2,16 @@ import axios from "axios";
 
 const soapClient = async (url, soapAction, soapBody) => {
   try {
-    const response = await axios.post(
-      url,
-      { soapAction, soapBody },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.post(url, soapBody, {
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        SOAPAction: soapAction,
+      },
+    });
 
     console.log(response);
 
-    return response.data.data;
+    return response.data;
   } catch (error) {
     console.error("SOAP request error:", error);
     throw error;
